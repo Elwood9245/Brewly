50c20ef3f1cc8d93ed6b5241c0aa169a1da76445
Junzhe Wu <2974710w@student.gla.ac.uk>
2025-07-12 17:17:15 +0100

    feat. Initial Spring Boot backend

---
+++ src/main/java/xyz/elwoodwjz/brewlybackend/BrewlybackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.elwoodwjz.brewlybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewlybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewlybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/elwoodwjz/brewlybackend/BrewlybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.elwoodwjz.brewlybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewlybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
